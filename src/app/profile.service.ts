16898f72bc829421a6ee94083846730766b89547
KaChryssie <karerassie@gmail.com>
2020-11-08 09:45:03 +0200

    add the function to get data form github and add the personal key

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  
+private username : string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    console.log("service is now ready!");
+    this.username = 'KaChryssie'
+  }
+
+getProfileInfo() {
+  return this.http.get("https://api.github.com/users/" +  this.username + "access_token= 6215e047c9a212f13a2f4e9671d6b2bffe03ab19")
+}
+
+
 }

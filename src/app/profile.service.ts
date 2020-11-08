07333cbfef4a4069a9e91c876f1d4795c8d4b9a5
KaChryssie <karerassie@gmail.com>
2020-11-08 21:53:02 +0200

    change for the username

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 
 

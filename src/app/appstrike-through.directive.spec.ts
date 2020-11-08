92db16dfab4b2bbad2c929ea5010b744f635d3c2
KaChryssie <karerassie@gmail.com>
2020-11-08 21:54:20 +0200

    change the file

---
+++ src/app/appstrike-through.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppstrikeThroughDirective } from './appstrike-through.directive';
+
+describe('AppstrikeThroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppstrikeThroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});

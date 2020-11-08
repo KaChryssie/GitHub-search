a91386d847c38d057035f13fb2ab2d749fa84216
KaChryssie <karerassie@gmail.com>
2020-11-08 08:41:04 +0200

    add the personal token key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey : '6215e047c9a212f13a2f4e9671d6b2bffe03ab19'
 };
 
 /*

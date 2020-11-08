e0b261f07527a6c326f55c6e4c7cf67256a0aee0
KaChryssie <karerassie@gmail.com>
2020-11-08 19:56:56 +0200

    add the method getrepos and the update method

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
+
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +15,20 @@ export class ProfileService {
   
 private username : string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: Http) { 
     console.log("service is now ready!");
     this.username = 'KaChryssie'
   }
 
 getProfileInfo() {
-  return this.http.get("https://api.github.com/users/" +  this.username + "access_token= 6215e047c9a212f13a2f4e9671d6b2bffe03ab19")
+  return this.http.get("https://api.github.com/users/" + this.username + "?access_token=" + "6215e047c9a212f13a2f4e9671d6b2bffe03ab19").pipe(map(res => res.json()));
 }
 
+getProfileRepos() {
+  return this.http.get("https://api.github.com/users/" + this.username + "/repos?order=created&sort=asc?access_token=" + "6215e047c9a212f13a2f4e9671d6b2bffe03ab19").pipe(map(res => res.json()));
+}
 
+updateProfile(username:string){
+  this.username = username;
+}
 }
